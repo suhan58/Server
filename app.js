b478b8bfd6a43d7cc72ace9e06def33ac2dc48b2
Suhan Rayamajhi <83125898+suhan58@users.noreply.github.com>
2022-02-13 19:41:18 +0545

    refined code

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const app = express();
 
-app.use("/graphql",graphqlHTTP({
-
-}));
+app.use("/graphql", graphqlHTTP({}));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Server is running ${port}...`);
 });
 
-module.exports = server; 
+module.exports = server;
